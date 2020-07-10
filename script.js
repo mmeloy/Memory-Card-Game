d0884eba05e69c30dbffd6b98ee3564fc056b56d
Mark Meloy <meloymark@johndeere.com>
2020-07-10 13:43:17 -0500

    initla css and html settings pseudo code and html grid layout

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("js linked")
+// Restart Game Function button - resets and re-shuffles cards, 
+//a nice to have would be a "are you sure prompt"
+
+//Turn counter function to keep track of number of turns in current game
+//function for best score denotes lowest number of turns in a game during current session
+
+
+
+//all side one of cards should have same image / pic
+//side 2 of cards should have matching pairs of letters, numbers, images, etc.
+
+
+
+
+
+//SILVER ideas
+//allow user to select difficulty level
+//easy
+//hard
+//expert
+//are you crazy
+
+
+
+
+
+//GOLD
+//let user select number of cards for a game - mults of 2
+//successfully solving all matches reveals a picture, saying. etc on background behind cards
+
+
+
