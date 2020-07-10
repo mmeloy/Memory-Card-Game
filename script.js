ca74dbb812c46a03279b0d45c64d68f3be934a24
Mark Meloy <meloymark@johndeere.com>
2020-07-10 15:52:57 -0500

    more js, html, css updates,classes for boxes

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 //a nice to have would be a "are you sure prompt"
 
 //Turn counter function to keep track of number of turns in current game
-//function for best score denotes lowest number of turns in a game during current session
+//function for best score denotes lowest number of turns in a game during current session (nice to have)
 
 
 
 //all side one of cards should have same image / pic
 //side 2 of cards should have matching pairs of letters, numbers, images, etc.
 
+//need event listener for each card , "onclick" to flip card to side 2
+// second card is flipped, and function compares result of two flipped cards, if they are equal, clear the 2 card spaces and loop back to 
 
-
+//function to display / shuffle cards randomly into the grid array, two cards side 1 and side 2 will need to display in the same area
 
 
 //SILVER ideas
@@ -27,6 +29,23 @@
 //GOLD
 //let user select number of cards for a game - mults of 2
 //successfully solving all matches reveals a picture, saying. etc on background behind cards
+//add CSS3 flip card animation
+
+//allow for 2 players with alternating turns  
+const boxes = document.querySelectorAll('.box');
+const resetButton = document.querySelector('#reset')
+
+console.log(boxes)
 
 
+let gameState = ['x', 'x', 'x', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',]
+let side2card = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',]
+let counter = 0
+//console.log(gameState.length)
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+    gameState = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',]
+    counter = 0
+}
 
